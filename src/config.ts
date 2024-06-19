e015f6d4aadb0ca4ad65e1914e9b92678815595c
Shashank S Mayya <shashanksmayya@gmail.com>
2024-06-19 19:08:34 +0530

    portfolio update

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Shashank S Mayya | Portfolio';
+export const SITE_DESCRIPTION = 'Welcome to my portfolio website. Built using Astro Framework.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
